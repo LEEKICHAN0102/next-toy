388345bc06602170f47ef4f4eb88890c09eb96a2
Kichan <rlcks01537@gmail.com>
2023-12-22 13:00:13 +0900

    nextauth naver auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,12 +1,17 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import NaverProvider from "next-auth/providers/naver";
 
 export const authOptions = {
   providers: [
     GithubProvider({
-      clientId: '2cb71368ab1b55743d1f',
-      clientSecret: '84e2c20639967ed5cb1d8775b4508db024d94016',
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }),
+    NaverProvider({
+      clientId: process.env.NAVER_CLIENT_ID,
+      clientSecret: process.env.NAVER_CLIENT_SECRET
+    })
   ],
   secret : 'qwer1234'
 };
