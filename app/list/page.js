a0b540004939d48b02cf5847ad43c233a8f276a1
Kichan <rlcks01537@gmail.com>
2023-12-22 11:53:54 +0900

    build routing (static | dynamic) caching

--- app/list/page.js
+++ app/list/page.js
@@ -1,6 +1,8 @@
 import { connectDB } from "/util/database.js"
 import ListItem from "./ListItem";
 
+export const dynamic = "force-dynamic"; // static <=> dynamic 환경 변수 설정
+
 export default async function List() {
   const db = (await connectDB).db('forum');
   let result = await db.collection('post').find().toArray();
--- app/page.js
+++ app/page.js
@@ -4,5 +4,10 @@ export default function Home() {
     <div>
       안녕 난 nextJS 야
     </div>
+
+    // await fetch("/URL" , { cache: "no-store"}); | 실시간 데이터 중요할 때 no caching
+    // await fetch("/URL" , { cache: "force-cache"}); | 데이터를 caching 해두고 재사용
+    // await fetch("/URL") 기본 fetch는 cache 사용해줌 위와 동일함.
+    // await fetch("/URL" , { next : { revalidate : 60 }}) | revalidate.value 주기로 cache 데이터 갱신
   )
 }
